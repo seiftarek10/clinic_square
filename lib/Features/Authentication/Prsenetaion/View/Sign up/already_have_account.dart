90593942be707c0d44554cabb04b68449698aee7
Seif Tariq <SeifTariq@gmail.com>
2024-08-16 15:04:34 +0300

    add sign in route

--- lib/Features/Authentication/Prsenetaion/View/Sign up/already_have_account.dart
+++ lib/Features/Authentication/Prsenetaion/View/Sign up/already_have_account.dart
@@ -1,7 +1,9 @@
+import 'package:clinic_square/Core/Routes/routes_path_key.dart';
 import 'package:clinic_square/Core/Themes/colors.dart';
 import 'package:clinic_square/Core/Themes/fonts.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class AlreadyHaveAccountText extends StatelessWidget {
   const AlreadyHaveAccountText({
@@ -17,11 +19,11 @@ class AlreadyHaveAccountText extends StatelessWidget {
             children: <TextSpan>[
           TextSpan(
               text: 'Sign In',
-              style: Fonts.font16_400DarkBlue
-                  .copyWith(color: AppColors.mainBlue),
+              style:
+                  Fonts.font16_400DarkBlue.copyWith(color: AppColors.mainBlue),
               recognizer: TapGestureRecognizer()
                 ..onTap = () {
-                  
+                  GoRouter.of(context).push(Routes.signin);
                 })
         ]));
   }
