016d46717e75d29cd8f4721788f04d8291faab9a
Seif Tariq <SeifTariq@gmail.com>
2024-08-18 19:38:18 +0300

    finish search section in doctors page

--- lib/Core/Themes/colors.dart
+++ lib/Core/Themes/colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static Color mainBlue = const Color(0xff247CFF);
   static Color darkBlue = const Color(0xff27272C);
+  static final middleGrey = Colors.grey[500];
   static final appBackgroundGrey = Colors.grey[200];
 }
--- lib/Features/Doctor Consultation/Presentaion/View/Widgets/search_section.dart
+++ lib/Features/Doctor Consultation/Presentaion/View/Widgets/search_section.dart
@@ -1,8 +1,7 @@
+import 'package:clinic_square/Core/Themes/colors.dart';
 import 'package:clinic_square/Core/Themes/fonts.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-
-
 
 class SearchSection extends StatelessWidget {
   const SearchSection({super.key});
@@ -12,18 +11,37 @@ class SearchSection extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          flex: 4,
+          flex: 5,
           child: TextFormField(
             decoration: InputDecoration(
-              prefixIcon: Icon(EvaIcons.searchOutline),
-              hintText: 'Search',
-              hintStyle: Fonts.font16_400DarkBlue
-              
-              ),
+                contentPadding: EdgeInsets.zero,
+                prefixIcon: Icon(
+                  EvaIcons.searchOutline,
+                  size: 28,
+                  color: AppColors.middleGrey,
+                ),
+                hintText: 'Search',
+                hintStyle: Fonts.font16_400DarkBlue
+                    .copyWith(color: AppColors.middleGrey),
+                fillColor: AppColors.appBackgroundGrey,
+                filled: true,
+                enabledBorder: borderStyle(),
+                focusedBorder: borderStyle()),
           ),
         ),
-        const Expanded(child: SizedBox())
+        Expanded(
+            child: IconButton(
+          padding: EdgeInsets.zero,
+          icon: const Icon(Icons.filter_list_sharp, size: 32),
+          onPressed: () {},
+        ))
       ],
     );
+  }
+
+  OutlineInputBorder borderStyle() {
+    return OutlineInputBorder(
+        borderSide: BorderSide(color: AppColors.appBackgroundGrey!, width: 0),
+        borderRadius: BorderRadius.circular(10));
   }
 }
