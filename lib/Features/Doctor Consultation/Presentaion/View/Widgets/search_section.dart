4f1ccdf0305f73b1c3d87ef8fa4a9a345e1dbf48
Seif Tariq <SeifTariq@gmail.com>
2024-08-18 19:25:42 +0300

    add Eva Icons Package

--- lib/Features/Doctor Consultation/Presentaion/View/Widgets/search_section.dart
+++ lib/Features/Doctor Consultation/Presentaion/View/Widgets/search_section.dart
@@ -1,10 +1,29 @@
+import 'package:clinic_square/Core/Themes/fonts.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+
+
 
 class SearchSection extends StatelessWidget {
   const SearchSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      children: [
+        Expanded(
+          flex: 4,
+          child: TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(EvaIcons.searchOutline),
+              hintText: 'Search',
+              hintStyle: Fonts.font16_400DarkBlue
+              
+              ),
+          ),
+        ),
+        const Expanded(child: SizedBox())
+      ],
+    );
   }
 }
--- lib/Features/Doctor Consultation/Presentaion/View/doctors_consultation.dart
+++ lib/Features/Doctor Consultation/Presentaion/View/doctors_consultation.dart
@@ -2,6 +2,7 @@ import 'package:clinic_square/Core/Helpers/space.dart';
 import 'package:clinic_square/Core/Widgets/app_padding.dart';
 import 'package:clinic_square/Core/Widgets/header_bar.dart';
 import 'package:clinic_square/Core/Widgets/scrollable_widget.dart';
+import 'package:clinic_square/Features/Doctor%20Consultation/Presentaion/View/Widgets/search_section.dart';
 import 'package:flutter/material.dart';
 
 class DoctorsConsultaion extends StatelessWidget {
@@ -16,7 +17,9 @@ class DoctorsConsultaion extends StatelessWidget {
                 child: Column(
           children: [
             Space.verticalSpace18,
-            const HeaderBar()
+            const HeaderBar(),
+            Space.verticalSpace18,
+            const SearchSection()
             
             ],
         ))),
