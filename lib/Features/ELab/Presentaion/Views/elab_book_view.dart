b2fb5e2541e028f737e23882d78b19a2a3679a9e
Seif Tariq <SeifTariq@gmail.com>
2024-08-31 16:15:53 +0300

    finish E-lab Location in lab book page

--- lib/Features/ELab/Presentaion/Views/elab_book_view.dart
+++ lib/Features/ELab/Presentaion/Views/elab_book_view.dart
@@ -1,5 +1,8 @@
+import 'package:clinic_square/Core/Helpers/space.dart';
 import 'package:clinic_square/Core/Widgets/app_padding.dart';
+import 'package:clinic_square/Core/Widgets/header_bar.dart';
 import 'package:clinic_square/Core/Widgets/scrollable_widget.dart';
+import 'package:clinic_square/Features/ELab/Presentaion/Views/Widgets/elab_location.dart';
 import 'package:flutter/material.dart';
 
 class ElabBookView extends StatelessWidget {
@@ -7,12 +10,22 @@ class ElabBookView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return  SafeArea(
       child: Scaffold(
         body: AppScrollWidget(child: 
         AppPadding(child: 
-        Column())),
+        Column(
+          children: [
+            Space.topSpace20,
+            const HeaderBar(headerText: "Lab Name"),
+            Space.verticalSpace18,
+            const ELabLocation(),
+            Icon(Icons.work_history)
+            
+          ],
+        ))),
       ),
     );
   }
 }
+
