62fde460bf9c1e4bdb301e112ae757e7e9c4ddda
Seif Tariq <SeifTariq@gmail.com>
2024-09-07 23:23:09 +0300

    add create patient activity feature

---
+++ lib/Features/Patient Activity/Presentaion/View/patient_activity.dart
@@ -0,0 +1,13 @@
+import 'package:clinic_square/Core/Helpers/app_screen.dart';
+import 'package:flutter/material.dart';
+
+class PatientActivity extends StatelessWidget {
+  const PatientActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScreen(
+      child: Column(),
+    );
+  }
+}
