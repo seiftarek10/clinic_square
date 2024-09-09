860d5d56088b953763ed1ae1f5fe6125f2041756
Seif Tariq <SeifTariq@gmail.com>
2024-09-09 16:18:30 +0300

    apply app gesture detector on home category container

--- lib/Features/Home/Presentaion/View/Widgets/home_category.dart
+++ lib/Features/Home/Presentaion/View/Widgets/home_category.dart
@@ -1,42 +1,55 @@
 import 'package:clinic_square/Core/Themes/colors.dart';
 import 'package:clinic_square/Core/Themes/fonts.dart';
+import 'package:clinic_square/Core/Widgets/app_gesture_detector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeCategory extends StatelessWidget {
-  const HomeCategory(
-      {super.key,
-      required this.category,
-      this.imageFlex,
-      this.textFlex,
-      required this.categoryIcon,
-      required this.onTap});
+class HomeCategory extends StatefulWidget {
+  const HomeCategory({
+    super.key,
+    required this.category,
+    this.imageFlex,
+    this.textFlex,
+    required this.categoryIcon,
+    required this.routePage,
+  });
 
-  final String category, categoryIcon;
+  final String category, categoryIcon, routePage;
   final int? imageFlex, textFlex;
-  final void Function() onTap;
 
   @override
+  State<HomeCategory> createState() => _HomeCategoryState();
+}
+
+class _HomeCategoryState extends State<HomeCategory> {
+  Color _color = AppColors.appBackgroundGrey!;
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
+    return AppGestureDetector(
+      route: widget.routePage,
+      onTapDownColor: Colors.grey[400]!,
+      onTapUpColor: AppColors.appBackgroundGrey!,
+      onColorChanged: (color) {
+        setState(() {
+          _color = color;
+        });
+      },
       child: Container(
         width: 102.w,
         height: 120.h,
         decoration: BoxDecoration(
-            color: AppColors.appBackgroundGrey,
-            borderRadius: BorderRadius.circular(15)),
+            color: _color, borderRadius: BorderRadius.circular(15)),
         child: Column(
           children: [
             SizedBox(height: 5.h),
             Expanded(
-                flex: imageFlex ?? 2,
-                child: Image.asset(categoryIcon, fit: BoxFit.cover)),
+                flex: widget.imageFlex ?? 2,
+                child: Image.asset(widget.categoryIcon, fit: BoxFit.cover)),
             SizedBox(height: 10.h),
             Expanded(
-                flex: textFlex ?? 1,
+                flex: widget.textFlex ?? 1,
                 child: Text(
-                  category,
+                  widget.category,
                   textAlign: TextAlign.center,
                   style: Fonts.font14_700darkBlue,
                 ))
